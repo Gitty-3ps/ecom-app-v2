46a6923bab7a7565dc134dab1599a6f907398213
gitty-3ps <deronhutchofficial77@gmail.com>
2025-08-07 13:50:11 -0500

    feat(home-page): add styling to website title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
     return (
         <nav className="sticky top-0 z-50 bg-white shadow">
             <div className="container mx-auto flex items-center justify-between px-4 py-4">
-                <Link href="/">
+                <Link href="/" className="hover:text-blue-600">
                     Ecommerce Store
                 </Link>
                 <div className="hidden md:flex space-x-6">
